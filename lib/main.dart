643855273eb8c640f10c8a3d6972f3324bf0b868
adhamjonakmalov <akmalovadham537@gmail.com>
2023-08-19 11:13:56 +0500

    Border

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,131 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  runApp(MaterialApp(
+    home: Container(
+      color: Color.fromARGB(255, 205, 96, 88),
+      child: Column(
+        children: [
+          Container(
+            margin:const EdgeInsets.all(30),
+            height: 70,
+            width: 380,
+            decoration:const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end:Alignment.bottomRight,
+                colors: [Colors.blue, Colors.black],
+               ),
+              
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Text('Eleveted Bottom',style: TextStyle(
+              decoration: TextDecoration.none,
+              color: Colors.green,
+              fontSize: 30
+            ),),
+
+             alignment: Alignment.center,
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            width: 380,
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.pink,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+              )
             ),
-          ],
-        ),
+            child: Text('Search',style: TextStyle(
+              decoration: TextDecoration.none,
+              color: Colors.red
+            ),),
+            alignment: Alignment.center,
+          ),
+          Container(
+            
+            margin: EdgeInsets.all(30),
+            width: 380,
+            height: 80,
+            child: Text('Start',style: TextStyle(
+              decoration: TextDecoration.none,
+              color: Colors.white,
+              fontSize: 50,
+
+            ),),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 220, 19, 5),
+              borderRadius: BorderRadius.only(       
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              )
+            ),
+            alignment: Alignment.center,
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            width: 380,
+            height: 80,
+            
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.red,
+                width: 5,
+              ),
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(60),
+                bottomRight: Radius.circular(60),
+              )
+              
+            ),
+            child: Text('Lalout',style: TextStyle(
+              decoration: TextDecoration.none,
+              color: const Color.fromARGB(255, 241, 23, 7),
+            ),),
+            alignment: Alignment.center,
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            width: 380,
+            height: 60,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color.fromARGB(255, 119, 10, 203),
+                width: 5,
+              ),
+              color: Colors.brown,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ) 
+            ),
+            child: Text('Border',style: TextStyle(
+              decoration: TextDecoration.none,
+              color: const Color.fromARGB(255, 217, 79, 70),
+              fontSize: 40,
+            ),),
+            alignment: Alignment.center,
+          ),
+
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  ));
 }
+
